510b016217c10672d74416e93a8a43a89db19efc
max <1733169585a@gmail.com>
2022-12-15 21:32:40 +0800

    Add empty_cache in batch

--- try.py
+++ try.py
@@ -1,9 +1,28 @@
 import torch
+import torch.nn as nn
+import numpy as np
 
-x = torch.tensor([1, 2, 3])
-y = torch.tensor([4, 5, 6])
+# x = torch.tensor([1, 2, 3])
+# y = torch.tensor([4, 5, 6])
+#
+# grid_x, grid_y = torch.meshgrid(x, y, indexing="ij")  # vs indexing="xy"
+#
+# print(grid_x)
+# print(grid_y)
 
-grid_x, grid_y = torch.meshgrid(x, y, indexing="ij")  # vs indexing="xy"
+input = torch.randn(1, 128, 160, 160)
 
-print(grid_x)
-print(grid_y)
+
+class Test(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.cv = nn.Conv2d(128, 256, 3, 2, 1)
+
+    def forward(self, x):
+        return self.cv(x)
+
+
+test = Test()
+output = test(input)
+
+print(output.shape)
