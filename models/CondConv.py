13fec83d14d7dbc88bc0e3cd01ed6176ec36e3c0
max <1733169585a@gmail.com>
2023-01-18 21:58:29 +0800

    Add StripConv in CondConv

--- models/CondConv.py
+++ models/CondConv.py
@@ -32,7 +32,7 @@ class Attention(nn.Module):
 
 
 class CondConv(nn.Module):
-    def __init__(self, in_planes, out_planes, kernel_size, stride, padding=0, dilation=1, grounps=1, bias=True,
+    def __init__(self, in_planes, out_planes, kernel_size, stride, padding=(0, 0), dilation=1, groups=1, bias=True,
                  num_exp=4,
                  init_weight=True):
         super().__init__()
@@ -42,13 +42,13 @@ class CondConv(nn.Module):
         self.stride = stride
         self.padding = padding
         self.dilation = dilation
-        self.groups = grounps
+        self.groups = groups
         self.bias = bias
         self.num_exp = num_exp
         self.init_weight = init_weight
         self.attention = Attention(in_planes=in_planes, K=num_exp, init_weight=init_weight)
 
-        self.weight = nn.Parameter(torch.randn(num_exp, out_planes, in_planes // grounps, kernel_size, kernel_size),
+        self.weight = nn.Parameter(torch.randn(num_exp, out_planes, in_planes // groups, kernel_size[0], kernel_size[1]),
                                    requires_grad=True)
         self.bn = nn.BatchNorm2d(out_planes)
         self.act = nn.ReLU()
@@ -73,15 +73,18 @@ class CondConv(nn.Module):
         Combination
         """
         aggregate_weight = torch.mm(softmax_att, weight).view(bs * self.out_planes, self.in_planes // self.groups,
-                                                              self.kernel_size, self.kernel_size)  # bs*out_p,in_p,k,k
+                                                              self.kernel_size[0],
+                                                              self.kernel_size[1])  # bs*out_p,in_p,k,k
 
         if self.bias is not None:
             bias = self.bias.view(self.num_exp, -1)  # K,out_p
             aggregate_bias = torch.mm(softmax_att, bias).view(-1)  # bs,out_p
-            output = F.conv2d(x, weight=aggregate_weight, bias=aggregate_bias, stride=self.stride, padding=self.padding,
+            output = F.conv2d(x, weight=aggregate_weight, bias=aggregate_bias, stride=self.stride,
+                              padding=(self.padding[0], self.padding[1]),
                               groups=self.groups * bs, dilation=self.dilation)
         else:
-            output = F.conv2d(x, weight=aggregate_weight, bias=None, stride=self.stride, padding=self.padding,
+            output = F.conv2d(x, weight=aggregate_weight, bias=None, stride=self.stride,
+                              padding=(self.padding[0], self.padding[1]),
                               groups=self.groups * bs, dilation=self.dilation)
 
         output = output.view(bs, self.out_planes, h, w)
@@ -90,7 +93,7 @@ class CondConv(nn.Module):
 
 if __name__ == '__main__':
     input = torch.randn(1, 128, 160, 160)
-    m = CondConv(in_planes=128, out_planes=256, kernel_size=7, stride=1, padding=3, bias=False, num_exp=4)
+    m = CondConv(in_planes=128, out_planes=256, kernel_size=(1, 7), stride=1, padding=(0, 3), bias=False, num_exp=4)
     start = time.time()
     out = m(input)
     end = time.time()
--- try.py
+++ try.py
@@ -10,14 +10,17 @@ import numpy as np
 # print(grid_x)
 # print(grid_y)
 
-x = torch.randn(64, 128)
-y = torch.randn(128, 256)
-
-out = torch.mm(x, y).view(-1)
-print(out.shape)
+# x = torch.randn(64, 128)
+# y = torch.randn(128, 256)
+#
+# out = torch.mm(x, y).view(-1)
+# print(out.shape)
 
 
 # test = Test()
 # output = test(input)
 #
 # print(output.shape)
+
+kernel = (7, 1)
+print(kernel[0])
